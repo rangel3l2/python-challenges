5cd44d3f9aadcf55a8ddafec3c045c46d7d4e3b4
Rafael Moreira <rafaelmoreira.at@hotmail.com>
2023-06-28 03:25:58 -0400

    Reorganização de pastas e alterações no main.py

--- challenge_01/main.py
+++ challenge_01/main.py
@@ -19,13 +19,13 @@ for escola in escolas:
         navegador.get("https://www.google.com")
         navegador.find_element(By.XPATH, '//*[@id="APjFqb"]').clear()
         navegador.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(escola + Keys.RETURN)
-        colegio.nome_escola = navegador.find_element(By.CSS_SELECTOR, ".qrShPb").text
-        colegio.endereco_escola = navegador.find_element(By.CSS_SELECTOR, ".LrzXr").text
-        lista_escolas.append(colegio)
+        nome_escola = navegador.find_element(By.CSS_SELECTOR, ".qrShPb").text
+        endereco_escola = navegador.find_element(By.CSS_SELECTOR, ".LrzXr").text
+        lista_escolas.append(colegio(nome_escola=nome_escola, endereco_escola=endereco_escola))
     except:
         print('Deu ruim')
 
-with open('./teste.csv', 'w', newline='') as csvfile:
+with open('teste.csv', 'w', newline='') as csvfile:
     csv.writer(csvfile).writerow(["Nome da Escola", "Endereço"])
     for escola in lista_escolas:
         csv.writer(csvfile).writerow([escola.nome_escola, escola.endereco_escola])
