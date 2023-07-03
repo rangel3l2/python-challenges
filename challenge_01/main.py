035c79a63105b1f2096f3e226bcbfd5b7a0cc1f8
rafaelmoreira.at@hotmail.com <rafaelmoreira.at@hotmail.com>
2023-07-03 14:21:02 -0400

    Implementação de pesquisa concluída

--- challenge_01/main.py
+++ challenge_01/main.py
@@ -1,5 +1,6 @@
 import csv
 import escola
+import time
 
 from selenium import webdriver
 from selenium.webdriver import Keys
@@ -7,31 +8,39 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+def get_column_of_csv(filename, column):
+    with open("./challenge_01/base_dados_teste.csv", "r", encoding='utf8') as stream:
+        reader = csv.DictReader(stream)
+        for row in reader:
+            yield row[column]
+
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 colegio = escola.Escola
-
-escolas = ["Escola Municipal Prof. Ramez Tebet", "Escola Estadual Fernando Corrêa", "elite tres lagoas"]
 lista_escolas = []
+escolas = ["Escola Test"]
 
+for name in get_column_of_csv('./challenge_01/base_dados_teste.csv', "Nome da Escola"):
+    escolas.append(name)
+    
+for escola in escolas:
+    print(escola)
+    
 for escola in escolas:
     try:
         navegador.get("https://www.google.com")
         navegador.find_element(By.XPATH, '//*[@id="APjFqb"]').clear()
-        navegador.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(escola + Keys.RETURN)
+        navegador.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(escola.format(str) + Keys.RETURN)
         nome_escola = navegador.find_element(By.CSS_SELECTOR, ".qrShPb").text
         endereco_escola = navegador.find_element(By.CSS_SELECTOR, ".LrzXr").text
         lista_escolas.append(colegio(nome_escola=nome_escola, endereco_escola=endereco_escola))
+        
     except:
-        print('Deu ruim')
+        print('Erro!')
 
-with open('teste.csv', 'w', newline='') as csvfile:
+with open('challenge_01/base_nova.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csv.writer(csvfile).writerow(["Nome da Escola", "Endereço"])
     for escola in lista_escolas:
         csv.writer(csvfile).writerow([escola.nome_escola, escola.endereco_escola])
-
-
-
-
-
-
+        
+            
