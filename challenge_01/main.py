eb0aa5f8f8bf592ea1b6feb110137125365c2847
rafaelmoreira.at@hotmail.com <rafaelmoreira.at@hotmail.com>
2023-07-04 14:43:12 -0400

    Ajustes no código: adição de array com o nome das colunas

--- challenge_01/main.py
+++ challenge_01/main.py
@@ -9,37 +9,39 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 #Funcao para pegar uma coluna específica no .csv
-def get_column_of_csv(filename, column):
+def get_column_of_csv(filename, column=[]):
     with open(filename, "r", encoding='utf8') as stream:
         reader = csv.DictReader(stream, delimiter=";")
         for row in reader:
-            yield row[column]
+            yield row[column[0]] + ' ' + row[column[1]] + ' ' + row[column[2]]
 
-service = Service(ChromeDriverManager().install())
-browser = webdriver.Chrome(service=service)
-school = sch.School
+srv = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=srv)
+schl = sch.School
 
 schools_list = []
 schools_not_found = []
 schools = []
+keys = ["Município", "UF", "Escola"]
 
 i = 0
-for name in get_column_of_csv('./challenge_01/files/escolas_ms.csv', 'Escola'):
+for name in get_column_of_csv('./challenge_01/files/escolas_ms.csv', keys):
     schools.append(name)
     i += 1
-    if(i==5):
+    if(i==50):
         break
     
+print(schools)
 # Trecho de automação das pesquisas no Google
-for school in schools:
+for item in schools:
     try:
         browser.get("https://www.google.com")
         browser.find_element(By.XPATH, '//*[@id="APjFqb"]').clear()
-        browser.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(school.format(str) + Keys.RETURN)
+        browser.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(item + Keys.RETURN)
         name = browser.find_element(By.CSS_SELECTOR, ".qrShPb").text
         adress = browser.find_element(By.CSS_SELECTOR, ".LrzXr").text
         telephone = browser.find_element(By.CSS_SELECTOR, '.zdqRlf').text
-        schools_list.append(school(name=name, adress=adress, telephone=telephone, status=True))
+        schools_list.append(schl(name=name, adress=adress, telephone=telephone))
         #time.sleep(3)
     except:
         name = browser.find_element(By.XPATH, '//*[@id="APjFqb"]').text
@@ -48,13 +50,11 @@ for school in schools:
 # Aqui salva o array das buscas realizadas com sucesso
 with open('challenge_01/files/base_nova.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csv.writer(csvfile).writerow(["Nome da Escola", "Endereço", "Telefone", "Status"])
-    for school in schools_list:
-        csv.writer(csvfile).writerow([school.name, school.adress, school.telephone, school.status])
+    for item in schools_list:
+        csv.writer(csvfile).writerow([item.name, item.adress, item.telephone])
         
 # Aqui salva o array das buscas sem sucesso
 with open('challenge_01/files/escolas_nao_salva.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csv.writer(csvfile).writerow(["Nome da Escola"])
-    for school in schools_not_found:
-        csv.writer(csvfile).writerow([school])
-        
-        
+    for item in schools_not_found:
+        csv.writer(csvfile).writerow([item])
--- challenge_01/models/school.py
+++ challenge_01/models/school.py
@@ -1,6 +1,5 @@
 class School:
-    def __init__(self, name, adress, telephone, status):
+    def __init__(self, name, adress, telephone):
         self.name = name
         self.adress = adress
         self.telephone = telephone
-        self.status = status
