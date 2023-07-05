91b09c1552db9bab5f2888b0c57b83719b42902e
rafaelmoreira.at@hotmail.com <rafaelmoreira.at@hotmail.com>
2023-07-05 18:19:28 -0400

    Necessario revisão -utils_function.py

--- challenge_01/utils/utils_function.py
+++ challenge_01/utils/utils_function.py
@@ -25,13 +25,19 @@ schools = []
 keys = ["Escola", "Município", "UF"]
 ximbinha = []
    
+def get_one_column_of_csv(filename, column1):
+    with open(filename, "r", encoding='utf8') as stream:
+        reader = csv.DictReader(stream, delimiter=",")
+        for row in reader:
+            yield row[column1]
+            
 def get_column_of_csv(filename, column=[]):
     with open(filename, "r", encoding='utf8') as stream:
         reader = csv.DictReader(stream, delimiter=";")
         for row in reader:
             yield row[column[0]] + ' ' + row[column[1]] + ' ' + row[column[2]]
 
-def get_column_of_csv(filename, column1, column2):
+def get_columns_of_csv(filename, column1, column2):
     with open(filename, "r", encoding='utf8') as stream:
         reader = csv.DictReader(stream, delimiter=",")
         for row in reader:
@@ -42,7 +48,7 @@ def google_search():
     for name in get_column_of_csv('./challenge_01/files/base_escolas_inep.csv', keys):
         schools.append(name)
         i += 1
-        if(i==1):
+        if(i==3):
             break
         
     for item in schools:
@@ -60,12 +66,12 @@ def google_search():
             schools_not_found.append(name)
 
 def generate_csv():
-    with open('challenge_01/files/escolas_brasil_att.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open(path_to_csv + 'escolas_brasil_att.csv', 'w', newline='', encoding='utf-8') as csvfile:
         csv.writer(csvfile).writerow(["Nome da Escola", "Endereço", "Telefone", "Status"])
         for item in schools_list:
             csv.writer(csvfile).writerow([item.name, item.adress, item.telephone])
             
-    with open('challenge_01/files/escolas_nao_salva.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open(path_to_csv + 'escolas_nao_salva.csv', 'w', newline='', encoding='utf-8') as csvfile:
         csv.writer(csvfile).writerow(["Nome da Escola"])
         for item in schools_not_found:
             csv.writer(csvfile).writerow([item])
@@ -82,11 +88,11 @@ def extract_info_by_maps_google():
             csv.writer(csvfile).writerow([item.attrib.get('aria-label')])
             
 def find_in_maps_google():
-    ximbinha = get_column_of_csv("./challenge_01/files/municipios.csv", keys[1], keys[2])
+    results = get_columns_of_csv(path_to_csv + 'municipios.csv', keys[1], keys[2])
     j = 0
     browser.get("https://www.google.com/maps")
     
-    for item in ximbinha:
+    for item in results:
         browser.find_element(By.ID, "searchboxinput").send_keys("escolas públicas em " + item + Keys.RETURN)
         time.sleep(2)
         coluna_escolas = browser.find_element(By.XPATH, "/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]")
@@ -103,5 +109,46 @@ def find_in_maps_google():
         browser.find_element(By.ID, "searchboxinput").clear()
         
         j += 1
-        if (j==2):
+        if (j==3):
             break
+        
+def search_school_founds_in_google_maps():
+    list_schools_database = []
+    list_schools_google_maps = []
+    schools_list.clear()
+    schools_not_found.clear()
+    
+    for name in get_one_column_of_csv(path_to_csv + 'escolas_brasil_att.csv', 'Nome da Escola'):
+        list_schools_database.append(name)
+            
+    for name in get_one_column_of_csv(path_to_csv + 'schools_found_google_maps.csv', 'Nome da Escola'):
+        list_schools_google_maps.append(name)
+        
+    # Necessario revisar
+    count = 0
+    for item_school_google_maps in list_schools_google_maps:
+        for item_school_database in list_schools_database:
+            if(item_school_google_maps == item_school_database):
+                count += 1
+        if(count == 0):
+            try:
+                browser.get("https://www.google.com")
+                browser.find_element(By.XPATH, '//*[@id="APjFqb"]').clear()
+                browser.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(item_school_google_maps + Keys.RETURN)
+                name = browser.find_element(By.CSS_SELECTOR, ".qrShPb").text
+                adress = browser.find_element(By.CSS_SELECTOR, ".LrzXr").text
+                telephone = browser.find_element(By.CSS_SELECTOR, '.zdqRlf').text
+                schools_list.append(schl(name=name, adress=adress, telephone=telephone))
+                #time.sleep(1)
+            except:
+                name = browser.find_element(By.XPATH, '//*[@id="APjFqb"]').text
+                schools_not_found.append(name)
+            finally:
+                count = 0
+        else:
+            break
+    
+    print(schools_list)
+    print(schools_not_found)
+                    
+
